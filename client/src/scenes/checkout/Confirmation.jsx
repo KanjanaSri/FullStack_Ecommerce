a7412c86f5d77d636b8ca76c6547dbb60688f561
SKarnjana <ksrisumrarn.pam@gmail.com>
2023-09-23 15:55:24 -0700

    add payment

--- client/src/scenes/checkout/Confirmation.jsx
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -1,5 +1,15 @@
+import { Box, Alert, AlertTitle } from "@mui/material";
+
 const Confirmation = () => {
-  return <div>Confirmation</div>;
+  return (
+    <Box m="90px auto" width="80%" height="50vh">
+      <Alert severity="Sucess">
+        <AlertTitle>Sucess</AlertTitle>
+        You have sucessfully made an Order -{" "}
+        <strong> Congrats on making your purchase</strong>
+      </Alert>
+    </Box>
+  );
 };
 
 export default Confirmation;
